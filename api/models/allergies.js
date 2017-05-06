8997c2aaefa59a29891dad7b29c84b00859a0ff0
csvinhal <cristiano.vinhal10@gmail.com>
2017-05-06 15:17:20 -0300

    add allergies

---
+++ api/models/allergies.js
@@ -0,0 +1,14 @@
+/**
+ * Created by crist on 06/05/2017.
+ */
+/**
+ * Created by crist on 06/05/2017.
+ */
+const restful = require('node-restful');
+const mongoose = restful.mongoose;
+
+const allergies = new mongoose.Schema({
+  name: {type: String, required: true}
+});
+
+module.exports = restful.model('Allergies', allergies);
